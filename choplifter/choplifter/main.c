d42dbbfcbd7fd18c2b4e2593a90086b25b4191d3
jeromepin <jeromepin38@gmail.com>
2012-12-04 11:08:33 +0100

    First Commit on 4/12

---
+++ choplifter/choplifter/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  choplifter
+//
+//  Created by Jerome on 04/12/12.
+//  Copyright (c) 2012 Jerome. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
